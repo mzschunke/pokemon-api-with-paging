c576df0210a8e3aa2cc1a7ca715866944f9377dc
Moritz Zschunke <moritz_z17@web.de>
2023-04-12 12:59:48 +0200

    solved

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -2,27 +2,37 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [offset, setOffset] = useState(1);
 
   useEffect(() => {
     async function loadPokemon() {
       try {
         const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
+          `https://pokeapi.co/api/v2/pokemon?offset=${offset}`
         );
+      //  console.log(response);
         const data = await response.json();
-        setPokemon(data.results);
+        console.log(data);
+        setPokemon(data.results); 
+       // console.log(data.results); 
       } catch (error) {
         console.log(error);
       }
     }
 
     loadPokemon();
-  }, []);
+  }, [offset]);
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button type="button" onClick={() => {
+        if (offset > 1) {
+        setOffset(offset - 1);
+      }
+    }}>Previous Page</button>
+
+
+      <button type="button" onClick={() => setOffset(offset + 1)}>Next Page</button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
@@ -31,3 +41,7 @@ export default function PokemonList() {
     </main>
   );
 }
+
+
+
+
